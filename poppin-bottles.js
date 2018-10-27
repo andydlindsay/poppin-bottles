e0a285ad1d3980e75bc5456228bc895dc5ebe780
Andy Lindsay <andydlindsay@gmail.com>
2018-10-27 00:49:42 +0000

    completed tasks 3 and 4

--- poppin-bottles.js
+++ poppin-bottles.js
@@ -1,20 +1,40 @@
 var totalBottles = function(input) {
   // initial purchase
-  var totalBottles = Math.floor(input / 2);
-  var empties = totalBottles;
-  var caps = totalBottles;
+  var totalBottles = Math.floor(input / 2) || 0;
 
-  while (empties > 1) {
-    var newBottles = Math.floor(empties / 2) + Math.floor(caps / 4);
-    empties = (empties % 2) + newBottles;
-    caps = (caps % 4) + newBottles;
-    totalBottles += newBottles;
+  var bottleObj = {
+    totalBottles: totalBottles,
+    purchasedBottles: totalBottles,
+    fromEmpties: 0,
+    fromCaps: 0,
+    empties: totalBottles,
+    caps: totalBottles
+  };
+
+  while (bottleObj.empties > 1) {
+    var fromEmpties = Math.floor(bottleObj.empties / 2);
+    var fromCaps = Math.floor(bottleObj.caps / 4);
+    var newBottles = fromEmpties + fromCaps;
+    bottleObj.fromEmpties += fromEmpties;
+    bottleObj.fromCaps += fromCaps;
+    bottleObj.empties = (bottleObj.empties % 2) + newBottles;
+    bottleObj.caps = (bottleObj.caps % 4) + newBottles;
+    bottleObj.totalBottles += newBottles;
   }
 
-  return totalBottles;
+  return bottleObj;
 };
 
 module.exports = totalBottles;
 
+var formatOutput = function(bottleObj) {
+  console.log(`TOTAL BOTTLES: ${bottleObj.totalBottles}`);
+  console.log(`REMAINING BOTTLES: ${bottleObj.empties}`);
+  console.log(`REMAINING CAPS: ${bottleObj.caps}`);
+  console.log('TOTAL EARNED:');
+  console.log(`  BOTTLES: ${bottleObj.fromEmpties}`);
+  console.log(`  CAPS: ${bottleObj.fromCaps}`);
+};
+
 var moneySpent = process.argv[2];
-console.log(`TOTAL BOTTLES: ${totalBottles(moneySpent)}`);
+formatOutput(totalBottles(moneySpent));
--- test/test.js
+++ test/test.js
@@ -5,25 +5,25 @@ describe("Poppin' Bottles", () => {
 
   it('should return 15 when investing $10', () => {
     var input = 10;
-    var result = totalBottles(input);
+    var result = totalBottles(input).totalBottles;
     assert.equal(result, 15);
   });
 
   it('should return 35 when investing $20', () => {
     var input = 20;
-    var result = totalBottles(input);
+    var result = totalBottles(input).totalBottles;
     assert.equal(result, 35);
   });
 
   it('should return 55 when investing $30', () => {
     var input = 30;
-    var result = totalBottles(input);
+    var result = totalBottles(input).totalBottles;
     assert.equal(result, 55);
   });
 
   it('should return 75 when investing $40', () => {
     var input = 40;
-    var result = totalBottles(input);
+    var result = totalBottles(input).totalBottles;
     assert.equal(result, 75);
   });
 
